b1f5cf369fe8563f3fe63fbb96b1b2a8e583e1b2
NdirituG <denisndiritug@gmail.com>
2022-05-20 11:59:46 +0300

    printf project by Carolyne Mutemi and Denis Ndiritu

---
+++ _printf.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * _pritnf - produces output according to format
+ * @format: character string
+ * Return: number of characters printed
+ * (excluding the null byte used to end output to strings)
+ * check_for_specifiers - checks if there is valid format specifier
+ * @format: possible format specifier
+ */
+int _printf(const char *format, ...)
+{
+	int (*pfunc)(va_list, flags_t *);
+	const char *p;
+	va_list arguments;
+	flags_t flags = {0, 0, 0};
+
+	register int count = 0;
+
+	va_start(arguments, format);
+	if (!format || (format[0] == '%' && !format[1]))
+		return (-1);
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
+		return (-1);
+	for (p == format; *p; p++)
+	{
+		if (*p == '%')
+		{
+			p++;
+			if (*p == '%')
+			{
+				count += _putchar('%');
+				continue;
+			}
+			while (get_flag(*p, &flags))
+				p++;
+			pfunc = get_print(*p);
+			count += (pfunc)
+				? pfunc(arguments, &flags)
+				: _printf("%%%c", *p);
+		} else
+			count += _putchar(*p);
+	}
+	_putchar(-1);
+	va_end(arguments);
+	return (count);
+}
